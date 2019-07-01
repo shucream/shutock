a037667d43c8abae1a31b42104bdf66181ef4c5e
内田就也 <shuya.uchida@gmail.com>
2019-07-02 02:49:39 +0900

    validation追加

--- db/migrate/20190701150823_create_products.rb
+++ db/migrate/20190701150823_create_products.rb
@@ -1,8 +1,9 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
-      t.string :name
-      t.string :description
+      t.string :name, null: false, limit: 100, unique: true
+      t.text :description, limit: 500
+      t.integer :price, null: false
 
       t.timestamps
     end
