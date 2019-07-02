2c2b30040803c28224e36c2d3c32232a1b2a0511
内田就也 <shuya.uchida@gmail.com>
2019-07-02 18:01:13 +0900

    rubocop導入

--- spec/factories/product.rb
+++ spec/factories/product.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
-    factory :product, class: Product do
-        name { "test_product" }
-        description { "test description" }
-        price { 1000 }
-    end
+  factory :product, class: Product do
+    name { 'test_product' }
+    description { 'test description' }
+    price { 1000 }
+  end
 end
