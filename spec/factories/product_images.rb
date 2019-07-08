89efddc45f467dc95cc8631235997e7618d350f4
内田就也 <shuya.uchida@gmail.com>
2019-07-09 02:03:17 +0900

    factoryBotの画像の添付方法を変えたた

--- spec/factories/product_images.rb
+++ spec/factories/product_images.rb
@@ -2,8 +2,6 @@ FactoryBot.define do
   factory :product_image do
     name { Faker::Book.title }
     product { create(:product) }
-    after(:build) do |product_image|
-      product_image.image.attach(io: File.open(Rails.root.join('spec', 'support', 'assets', 'sample.jpg')), filename: 'sample.jpeg', content_type: 'image/jpg')
-    end
+    image { Rails.root.join('spec', 'support', 'assets', 'sample.jpg').open }
   end
 end
--- spec/factories/shop_images.rb
+++ spec/factories/shop_images.rb
@@ -1,8 +1,6 @@
 FactoryBot.define do
   factory :shop_image do
     shop { create(:shop) }
-    after(:build) do |shop_image|
-      shop_image.image.attach(io: File.open(Rails.root.join('spec', 'support', 'assets', 'sample.jpg')), filename: 'sample.jpeg', content_type: 'image/jpg')
-    end
+    image { Rails.root.join('spec', 'support', 'assets', 'sample.jpg').open }
   end
 end
