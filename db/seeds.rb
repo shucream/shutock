7c7a663bc1bac8bf166cf7a170a2f68b28f3623a
内田就也 <shuya.uchida@gmail.com>
2019-07-09 19:42:21 +0900

    seed修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,21 +3,24 @@ puts 'Start seeds...'
 DUMMY_SHOPS_COUNT = 10
 DUMMY_PRODUCTS_COUNT = 100
 
-DUMMY_SHOPS_COUNT.times do
-  Shop.create!(
-    name: Faker::Company.name,
+DUMMY_SHOPS_COUNT.times do |i|
+  shop = Shop.new(
+    name: i.to_s+Faker::Company.name,
     address: Faker::Address.full_address
   )
+  shop.shop_images.append(FactoryBot.build(:shop_image, shop: shop))
+  shop.save!
 end
 
-DUMMY_PRODUCTS_COUNT.times do
+DUMMY_PRODUCTS_COUNT.times do |i|
   product = Product.new(
-    name: Faker::Food.dish,
+    name: i.to_s+Faker::Food.dish,
     description: Faker::Food.description,
     price: Faker::Number.between(1, 10) * 100
   )
   shop = Shop.find(Faker::Number.between(1, DUMMY_SHOPS_COUNT))
   product.stocks.new(shop: shop, quantity: Faker::Number.between(1, 10))
+  product.product_images.append(FactoryBot.build(:product_image, product: product))
   product.save!
 end
 
