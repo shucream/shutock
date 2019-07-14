61dd0607020c71c96b8a6bc5724224a3f332f84c
内田就也 <shuya.uchida@gmail.com>
2019-07-15 02:00:13 +0900

    circleci2

---
+++ client/build/precache-manifest.c8dcf98d90e3d612fba1b7bc4e0eaba4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fe2c3886b52c30ed879f8734f88eca6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0447f605b782d456f4be",
+    "url": "/static/css/main.1f3c5447.chunk.css"
+  },
+  {
+    "revision": "8bb566734afa616fb484",
+    "url": "/static/js/2.f04323ce.chunk.js"
+  },
+  {
+    "revision": "0447f605b782d456f4be",
+    "url": "/static/js/main.bb7a9366.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
