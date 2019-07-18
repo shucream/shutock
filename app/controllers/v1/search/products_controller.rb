92dccd98cadf3ce11b437553fd45c57c781c5eb6
内田就也 <shuya.uchida@gmail.com>
2019-07-19 06:52:34 +0900

    検索追加

--- app/controllers/v1/search/products_controller.rb
+++ app/controllers/v1/search/products_controller.rb
@@ -1,3 +1,12 @@
 class V1::Search::ProductsController < ApplicationController
-  def index; end
+  def index
+    @result = Product.search_name(search_params[:q])
+    render json: @result, each_serializer: V1::ProductSerializer, include: {}
+  end
+
+  private
+
+  def search_params
+    params.permit(:q)
+  end
 end
